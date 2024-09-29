6128b8d0671bc89c2cd75caa568acaeb85140d03
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2024-09-29 22:41:53 +0200

    refactor: simplified dotenv reader module
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- dotenv-reader/src/index.ts
+++ dotenv-reader/src/index.ts
@@ -1,7 +1,16 @@
-import * as reader from "./env-reader.js";
 import dotenv from "dotenv";
+
+const get = (key: string): string => {
+  if (!(key in process.env) || typeof process.env[key] === "undefined") {
+    throw new Error(
+      `Key ${key} was not found in the env file. Did you spell it correctly?`
+    );
+  }
+
+  return process.env[key];
+};
 
 export default {
   loadEnvFile: dotenv.config,
-  get: reader.get
+  get
 };
