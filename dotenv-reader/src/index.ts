4a7ef1b3f893b7ba3581cd511de071985c8268ad
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2024-10-09 22:38:18 +0200

    Added dotenv reader package
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

---
+++ dotenv-reader/src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+const get = (key: string): string => {
+  if (!(key in process.env) || typeof process.env[key] === "undefined") {
+    throw new Error(
+      `Key ${key} was not found in the env file. Did you spell it correctly?`
+    );
+  }
+
+  return process.env[key];
+};
+
+export default {
+  loadEnv: dotenv.config,
+  get
+};
