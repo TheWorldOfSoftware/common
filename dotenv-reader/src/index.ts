0297792f68385dc722aecc6b596ee333c6a18c66
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2025-01-26 12:10:12 +0100

    Made get key check more strict
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- dotenv-reader/src/index.ts
+++ dotenv-reader/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 
 const get = (key: string): string => {
-  if (!(key in process.env) || typeof process.env[key] === "undefined") {
+  if (!(key in process.env) || typeof process.env[key] !== "string") {
     throw new Error(
       `Key ${key} was not found in the env file. Did you spell it correctly?`
     );
